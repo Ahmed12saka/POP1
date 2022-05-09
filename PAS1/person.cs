a0bcb81676d8d8646d165d97696f875ac0e6d0a6
User <User@DESKTOP-TOA5621>
2022-05-09 23:16:20 +0300

    Add project files.

---
+++ PAS1/person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAS1
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
